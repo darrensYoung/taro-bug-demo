8c03189b897d037a2e80299473b1cb8cf4d706ca
zoya <18333999370@163.com>
2023-06-11 16:02:35 +0800

    fix:bug

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -4,22 +4,15 @@ import {
   Button
 } from "@nutui/nutui-react-taro";
 import './index.scss'
-import Taro from '@tarojs/taro';
 
 function Index() {
-  const toPage = () => {
-    Taro.navigateTo({
-      url:'/pages/login/index'
-    })
-
-  }
   return (
     <View className="nutui-react-demo">
       <View className="index">
-        欢迎使用 NutUI React 开发 Taro 多端项目。
-      </View>
+        登录
+        </View>
       <View className="index">
-        <Button type="primary" className="btn" onClick={toPage}>
+        <Button type="primary" className="btn">
           NutUI React Button
         </Button>
       </View>
